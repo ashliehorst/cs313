68915fed8b6f600ead8f0125e6c490340aa86c7d
ashliehorst <ashliehorst@yahoo.com>
2015-01-24 22:55:13 -0700

    Save

--- data.php
+++ data.php
@@ -1,20 +1,20 @@
 <?php 
-$countLink = "0"; 
+$countLink = "1"; 
 $countZelda = "0"; 
 $countSup = "0"; 
 $countVil = "0" ?> 
 <?php 
 $countFru = "0"; 
-$countChoc = "0"; 
+$countChoc = "1"; 
 $countVan = "0"; 
 $countMin = "0" ?> 
 <?php 
 $countFor = "0"; 
 $countBea = "0"; 
-$countMou = "0"; 
+$countMou = "1"; 
 $countDes = "0" ?> 
 <?php 
 $countPop = "0"; 
 $countRoc = "0"; 
 $countCla = "0"; 
-$countTec = "0" ?> 
+$countTec = "1" ?> 
