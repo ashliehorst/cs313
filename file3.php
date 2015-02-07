ce89a08fb6464bf7b5fb328cb193a632870caf4e
ashliehorst <ashliehorst@yahoo.com>
2015-02-07 15:54:47 -0700

    Update file3.php

--- file3.php
+++ file3.php
@@ -1,64 +1,24 @@
 <?php
-function databaseConnection(){
-        $server = '127.7.209.2';
-        $username = 'afane';
-        $passwd = 'rareriroru';
-        $database = 'php';
-        $dsn = "mysql:host=$server; dbname=$database";
-        try{
-            $dataConn = new PDO($dsn, $username, $passwd); //creates a PDO Object
-        } catch (PDOException $exc) {
-                echo "<p>An error occurred while connecting to the database</p>";
-        }
-        
-        if(is_object($dataConn)){
-            return $dataConn;
-        }
-        else{
-            return FALSE;
-        }
+if(!$_SESSION){
+    session_start();
+ }
+require $_SERVER['DOCUMENT_ROOT'].'/dataBase/practice2.php';
+if ($_POST['action'] == "Search" && !empty($_POST['search'])){
+	$searchVariable = $_POST['search'];
+	$scriptures = getSpecificScriptures($searchVariable);
+	$output = "<h1>Scripture Resources</h1>";
+	foreach ($scriptures as $value) {
+		$output .= "<b>$value[1] $value[2]:$value[3]</b> - <br /><br />";
+	}
+	include "view.php";
+	exit;
 }
-function getScriptures(){
-$conn = databaseConnection();
-    
-    try{
-        $sql = 'SELECT * FROM products';
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $data = $stmt->fetchAll();
-        $stmt->closeCursor();
-            
-    } catch (PDOException $ex) {
-        echo 'getScriptures error';
-    }
-        if(is_array($data)){
-            return $data;
-        }
-        else{
-            return FALSE;
-        }
+else {
+	$scriptures = getScriptures();
+	$output = "<h1>Scripture Resources</h1>";
+	foreach ($scriptures as $value) {
+		$output .= "<b>$value[1] $value[2]:$value[3]</b> - <br /><br />";
+	}
+	include "view.php";
+	exit;
 }
-function getSpecificScriptures($searchVariable){
-$conn = databaseConnection();
-    
-    try{
-        $sql = 'SELECT * FROM products WHERE name = :name';
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue(':name', $searchVariable, PDO::PARAM_STR);
-        $stmt->execute();
-        $data = $stmt->fetchAll();
-        $stmt->closeCursor();
-            
-    } catch (PDOException $ex) {
-        echo 'getScriptures error';
-    }
-        if(is_array($data)){
-            return $data;
-        }
-        else{
-            return FALSE;
-        }
-}
-?>
