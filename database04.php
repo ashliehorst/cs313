6e95bf8d0d598da9c8f565b86cc8d17245879471
ashliehorst <ashliehorst@yahoo.com>
2015-02-07 15:46:33 -0700

    Update database04.php

--- database04.php
+++ database04.php
@@ -6,23 +6,52 @@
 <?php
 try
 {
-	$query = "Fisher";
-	$user = "ashliehorst";
-	$pass = "Soccermom1";
-	$db = new PDO("mysql:host=127.4.54.130;dbname=hobbyfun", $user, $pass);
-	echo "Connection established! :-)<br />";
+
+function loadDatabase()
+{
+
+  $dbHost = "127.4.54.130";
+  $dbPort = "";
+  $dbUser = "ashliehorst";
+  $dbPassword = "Soccermom1";
+
+     $dbName = "hobbyfun";
+
+    $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+          //echo "Using local credentials: "; 
+          require("setLocalDatabaseCredentials.php");
+     }
+     else 
+     { 
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbName = getenv('hobbyFun'); 
+          $dbUser = getenv('adminv5ijxEy');
+          $dbPassword = getenv('REtjuvjGyn6-');
+     } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 	
-	/*
-	$sql = "SELECT name FROM user WHERE name LIKE :name";
-	
-	$statement = $db->prepare($sql);
-	$statement->bindValue(':name', '%' . $query . '%', PDO::PARAM_STR);
-	$statement->execute();
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		echo "Found: " . $row["name"] . "<br />\n";
-	}
-	*/
+    $db = new PDO("mysql:host=$dbHost:dbname=$dbName", $dbUser, $dbPassword);
+
+     return $db;
+
+}
+
+$db = loadDatabase();
+
+$statement = $db->query('SELECT name FROM hobbies');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Hobbies</h2>";
+
+foreach($results as $hobby)
+{
+	echo "<p><b>" . $hobby['name'];
+}
+
 }
 catch (PDOException $ex)
 {
