9263411b6affdeec0056d0202d828772c453f3e2
ashliehorst <ashliehorst@yahoo.com>
2015-02-07 16:55:06 -0700

    Update file3.php

--- file3.php
+++ file3.php
@@ -5,19 +5,19 @@ if(!$_SESSION){
 require $_SERVER['DOCUMENT_ROOT'].'file1.php';
 if ($_POST['action'] == "Search" && !empty($_POST['search'])){
 	$searchVariable = $_POST['search'];
-	$scriptures = getSpecificScriptures($searchVariable);
-	$output = "<h1>Fun Hobbies</h1>";
-	foreach ($scriptures as $value) {
-		$output .= "<b>$value[1]</b> <br />";
+	$activities = getSpecificActivity($searchVariable);
+	$output = "<h1>Activities</h1>";
+	foreach ($activities as $value) {
+		$output .= "$value[1] <br />";
 	}
 	include "file2.php";
 	exit;
 }
 else {
-	$scriptures = getScriptures();
-	$output = "<h1>Fun Hobbies</h1>";
-	foreach ($scriptures as $value) {
-		$output .= "<b>$value[1]</b> <br />";
+	$activities = getActivities();
+	$output = "<h1>Activities</h1>";
+	foreach ($activities as $value) {
+		$output .= "$value[1] <br />";
 	}
 	include "file2.php";
 	exit;
