9398f108127a97a49e612c8ed11e30067e9bf1a1
ashliehorst <ashliehorst@yahoo.com>
2015-02-07 19:04:44 -0700

    Update file3.php

--- file3.php
+++ file3.php
@@ -1,28 +1,3 @@
-<?php
-if(!$_SESSION){
-    session_start();
- }
-require $_SERVER['DOCUMENT_ROOT'].'file1.php';
-if ($_POST['action'] == "Search" && !empty($_POST['search'])){
-	$searchVariable = $_POST['search'];
-	$activities = getSpecificActivity($searchVariable);
-	$output = "<h1>Activities</h1>";
-	foreach ($activities as $value) {
-		$output .= "$value[1] <br />";
-	}
-//	include "file2.php";
-	exit;
-}
-else {
-	$activities = getActivities();
-	$output = "<h1>Activities</h1>";
-	foreach ($activities as $value) {
-		$output .= "$value[1] <br />";
-	}
-//	include "file2.php";
-	exit;
-}
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,6 +29,33 @@ else {
       <footer>
         <p>&copy; Hobbies and Fun, Inc.</p>
       </footer>
+      
+      <?php
+if(!$_SESSION){
+    session_start();
+ }
+require $_SERVER['DOCUMENT_ROOT'].'file1.php';
+if ($_POST['action'] == "Search" && !empty($_POST['search'])){
+	$searchVariable = $_POST['search'];
+	$activities = getSpecificActivity($searchVariable);
+	$output = "<h1>Activities</h1>";
+	foreach ($activities as $value) {
+		$output .= "$value[1] <br />";
+	}
+//	include "file2.php";
+	exit;
+}
+else {
+	$activities = getActivities();
+	$output = "<h1>Activities</h1>";
+	foreach ($activities as $value) {
+		$output .= "$value[1] <br />";
+	}
+//	include "file2.php";
+	exit;
+}
+
+?>
 
 </body>
 </html>
